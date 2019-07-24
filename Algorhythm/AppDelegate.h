246ad04bc3d8d14c0ed045702d38574ff1aefe34
Austin Meyer <Austin.a.Meyer@gmail.com>
2019-07-23 19:42:02 -0500

    Initial Commit

---
+++ Algorhythm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorhythm
+//
+//  Created by Austin Meyer on 7/23/19.
+//  Copyright © 2019 Austin Meyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorhythm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorhythm
+//
+//  Created by Austin Meyer on 7/23/19.
+//  Copyright © 2019 Austin Meyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
